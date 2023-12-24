6583a70dc8322a3e429c33c5f4688203bd888f7e
Mary-Orlova <vampusha07@gmail.com>
2023-12-24 03:39:26 +0300

    correct section gallery modal and catalog almost

--- project/js/accordion-init.js
+++ project/js/accordion-init.js
@@ -7,45 +7,43 @@
 
 // // Тabs
 
-// const params = {
-//   tabsClass: "js-tab-btn",
-//   wrap: "js-tabs-wrap",
-//   content: "js-tab-content",
-//   active: "active"
-// };
+(() => {
+  const params = {
+    tabsClass: "js-tab-btn",
+    wrap: "js-tabs-wrap",
+    content: "js-tab-content",
+    active: "active"
+  };
 
-// function setTabs(params) {
-//   const tabBtns = document.querySelectorAll(`.${params.tabsClass}`);
+  function setTabs(params) {
+    const tabBtns = document.querySelectorAll(`.${params.tabsClass}`);
 
-//   function onTabClick(e) {
-//     e.preventDefault();
-//     const path = this.dataset.path;
-//     const wrap = this.closest(`.${params.wrap}`);
-//     const currentContent = wrap.querySelector(`.${params.content}[data-target="${path}"]`);
-//     const contents = wrap.querySelectorAll(`.${params.content}`);
+    function onTabClick(e) {
+      e.preventDefault();
+      const path = this.dataset.path;
+      const wrap = this.closest(`.${params.wrap}`);
+      const currentContent = wrap.querySelector(`.${params.content}[data-target="${path}"]`);
+      const contents = wrap.querySelectorAll(`.${params.content}`);
 
-//     contents.forEach((el) => {
-//       el.classList.remove(params.active);
-//     });
+      contents.forEach((el) => {
+        el.classList.remove(params.active);
+      });
 
-//     currentContent.classList.add(params.active);
+      currentContent.classList.add(params.active);
 
-//     tabBtns.forEach((el) => {
-//       el.classList.remove(params.active);
-//     });
+      tabBtns.forEach((el) => {
+        el.classList.remove(params.active);
+      });
 
-//     this.classList.add(params.active);
-//   }
+      this.classList.add(params.active);
+    }
 
-//   tabBtns.forEach(function (el) {
-//     el.addEventListener("click", onTabClick);
-//   });
+    tabBtns.forEach(function (el) {
+      el.addEventListener("click", onTabClick);
+    });
+  }
 
-// }
-
-// setTabs(params);
+  setTabs(params);
 
 
-// function scrollToTarget(){
-//   document.getElementById("tab").scrollIntoView();
-// }
+  })();
