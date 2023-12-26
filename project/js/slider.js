d2e9292ed4352820b5f51a99a2f257fa454ffa9b
Mary-Orlova <vampusha07@gmail.com>
2023-12-27 02:48:56 +0300

    correct events-section

--- project/js/slider.js
+++ project/js/slider.js
@@ -20,6 +20,7 @@
   });
 })();
 
+(() => {
 const gallerySlider = new Swiper(".js-gallery-slider", {
   slidesPerView: 1,
   grid: {
@@ -81,5 +82,40 @@ const gallerySlider = new Swiper(".js-gallery-slider", {
       });
     }
   }
+});
+})();
 
+
+
+(() => {
+  const eventsSwiper = new Swiper('.js-events-swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: ".events-next",
+      prevEl: ".events-prev"
+    },
+    pagination: {
+      el: '.events-swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 20
+      },
+      680: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 27
+      }
+    }
+  });
 })();
