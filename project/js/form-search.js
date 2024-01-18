bf0d61e8a7b0ec54d1812d6007d12c44d7913c99
Mary-Orlova <vampusha07@gmail.com>
2024-01-18 03:24:30 +0300

    add styles for search top header and continue adaptive 1080

--- project/js/form-search.js
+++ project/js/form-search.js
@@ -50,3 +50,24 @@
     hiddenClass: "is-closed" // класс закрывающегося состояния (удаляется сразу после закрытия)
   });
 })();
+
+document.querySelector('.header__search-btn-open').addEventListener('click', function () {
+  document.querySelector('.header__search-form').classList.add('form__active');
+  this.classList.add('active');
+})
+document.querySelector('.header__close-btn-top').addEventListener('click', function () {
+  let form = document.querySelector('.header__search-form');
+  form.classList.remove('form__active');
+  form.querySelector('input').value = '';
+  document.querySelector('.header__search-btn-open').classList.remove('active')
+});
+
+document.addEventListener('click', function (e) {
+  let target = e.target;
+  let form = document.querySelector('.header__search-form');
+  if (!target.closest('.header__top-search')) {
+    form.classList.remove('form__active');
+    form.querySelector('input').value = '';
+    document.querySelector('.header__search-btn-open').classList.remove('active')
+  }
+})
