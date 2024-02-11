987867ef8c38a0801375c0270f122e9c32a0b38b
Mary-Orlova <vampusha07@gmail.com>
2024-02-11 23:31:26 +0300

    add pravki for header, h ero, gallery, projects, events, social, map, contacts and media

--- project/js/modal.js
+++ project/js/modal.js
@@ -2,6 +2,7 @@ const openModalButtons = document.querySelectorAll ('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll ('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
+
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
     const modal = document.querySelector(button.dataset.modalTarget)
@@ -28,11 +29,15 @@ function openModal(modal) {
   if (modal == null) return
   modal.classList.add('modal-active')
   overlay.classList.add('modal-active')
+  // запрет на скрол body, пока открыта страница модального окна
+  document.getElementsByTagName("body")[0].style.overflow = 'hidden';
 }
 
 function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('modal-active')
   overlay.classList.remove('modal-active')
+  // разрешение на скрол body, когда закрываем модальное окно
+  document.getElementsByTagName("body")[0].style.overflow = 'scroll';
 }
 
